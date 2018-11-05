c724dad52944ddd28354716e947c71c4707a5f22
dineshuser <44768611+dineshuser@users.noreply.github.com>
2018-11-05 19:44:08 +0530

    Python Assessment Test

---
+++ script.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+This script prints the input string for this script
+
+"""
+import sys
+
+__author__ = 'dinesh'
+
+if __name__ == "__main__":
+    input_text = sys.stdin.read()
+    print(input_text, end='')
